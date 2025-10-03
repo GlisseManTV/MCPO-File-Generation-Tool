ed6675d37c971ce78426efaf8634b1c901a3caa2
GlissemanTV <admin@itconsult.care>
2025-10-04 00:36:56 +0200

    fix too much uvicorn logs

--- LLM_Export/docker/sse_http/tools/file_export_mcp.py
+++ LLM_Export/docker/sse_http/tools/file_export_mcp.py
@@ -1544,6 +1544,7 @@ if __name__ == "__main__":
             app,
             host=host,
             port=port,
+            access_log=False,
             log_level="info",
             use_colors=False
         )
