d68e38c030e1d4d71b582492415967da6d75a32b
GlissemanTV <admin@itconsult.care>
2025-11-10 14:04:43 +0100

    chore(release): bump SCRIPT_VERSION to 0.8.1-rc4 in file_export_mcp
    Update version constant from 0.8.1-rc3 to 0.8.1-rc4 to prepare the next release candidate and keep the runtime-reported version in sync with packaging/deploy tags. No functional changes.

--- LLM_Export/docker/sse_http/tools/file_export_mcp.py
+++ LLM_Export/docker/sse_http/tools/file_export_mcp.py
@@ -58,7 +58,7 @@ from starlette.applications import Starlette
 from starlette.routing import Route, Mount
 from starlette.responses import Response, JSONResponse, StreamingResponse
 
-SCRIPT_VERSION = "0.8.1-rc3"
+SCRIPT_VERSION = "0.8.1-rc4"
 
 URL = os.getenv('OWUI_URL')
 TOKEN = os.getenv('JWT_SECRET') ## will be deleted in 1.0.0
