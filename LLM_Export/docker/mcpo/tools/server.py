3c59f5fc352dddc57456fc4824054e67d543b8bf
GlissemanTV <gaetan.aro@gmail.com>
2026-08-17 00:09:32 +0200

    chore: bump script version to 1.0.4-dev2
    Update SCRIPT_VERSION in both MCP and SSE HTTP server instances to
    indicate the second development iteration of version 1.0.4.

--- LLM_Export/docker/mcpo/tools/server.py
+++ LLM_Export/docker/mcpo/tools/server.py
@@ -63,7 +63,7 @@ from utils import (
 )
 from utils.pptx_treatment import _resolve_donor_simple
 
-SCRIPT_VERSION = "1.0.4-dev"
+SCRIPT_VERSION = "1.0.4-dev2"
 
 LOG_LEVEL_ENV = os.getenv("LOG_LEVEL")
 LOG_FORMAT_ENV = os.getenv("LOG_FORMAT", "%(asctime)s %(levelname)s %(name)s - %(message)s")
